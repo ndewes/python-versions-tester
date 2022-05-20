df03e128650d8bb8eaed61099dfc7acd9d318807
Niklas Dewes <niklas.dewes@outlook.com>
2022-05-20 09:49:19 +0200

    file edit

--- test_env.sh
+++ test_env.sh
@@ -4,5 +4,6 @@ imageDirectory="image_$now"
 mkdir tests/$imageDirectory
 
 for pyver in 3.6 3.7 3.8 3.9 3.10; do 
-    docker build --build-arg python_version="$pyver" --tag mytest:$pyver . | tee tests/$imageDirectory/output_$pyver.txt
+    outputFile="output_$pyver.txt"
+    docker build --build-arg python_version="$pyver" --tag mytest:$pyver . | tee tests/$imageDirectory/$outputFile
 done
